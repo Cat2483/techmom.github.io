3c438001a3b9b4b1f5a5b26ccfbd762668761fa0
Cat2483 <160343701+Cat2483@users.noreply.github.com>
2024-02-24 15:40:24 -0700

    Updated 2/24

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
-const red = 5;
-const green = 129;
-const blue = 59;
+const red = 135;
+const green = 203;
+const blue = 165;
 
 const header = document.getElementById("header");
-
+console.log(header.innerText)
 
 function changeColor() {
-
+    console.log(window.scrollY)
     const scrollFactor = 1 + window.scrollY / 400;
 
     const r = red / scrollFactor;
     const g = green / scrollFactor;
     const b = blue / scrollFactor;
 
-    const header = document.getElementById("header");
 
-    const updatedColor = `rgb (${r}, ${g}, ${b})`;
-
+    const updatedColor = `rgb(${r}, ${g}, ${b})`;
 
     header.style.backgroundColor = updatedColor;
 
-
-
 }
 changeColor()
-window.addEventListener('scroll', changeColor)
+
+window.addEventListener("scroll", changeColor)
 
